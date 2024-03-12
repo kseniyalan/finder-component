186755220d30da7c71cd11e8f13a8136f7dd2ce7
Ksenia Lanetskaia <kseniia.lanetskaia@scr.sk>
2024-03-12 20:31:44 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.scss'
+
+function App() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>My Finder component</div>
+    </>
+  )
+}
+
+export default App
